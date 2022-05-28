ca6442c238cfee4166d4412db365cfcfac3cf0bb
Phil Pepin <ppepin34@gmail.com>
2022-05-28 13:17:57 -0400

    can add department and role

--- index.js
+++ index.js
@@ -28,13 +28,19 @@ function mainMenu() {
                 viewRoles()
             } else if (answers.menu === 'View all employees') {
                 viewEmployees()
+            } else if (answers.menu === 'Add a department') {
+                addDepartment()
+            } else if (answers.menu === 'Add a role') {
+                addRole()
+            } else if (answers.menu === 'Add an employee') {
+                addEmployee()
             }
         })
 };
 
 function viewDepartments() {
 
-    let sql  = `SELECT department.id AS id, department.name AS 'Department' FROM department`;
+    let sql = `SELECT department.id AS id, department.name AS 'Department' FROM department`;
     db.query(sql, (error, response) => {
         if (error) throw error;
         console.table(response);
@@ -70,6 +76,86 @@ function viewEmployees() {
     ON r.department_id = d.id
     LEFT JOIN employee AS m
     ON e.manager_id = m.id`;
+
+    db.query(sql, (error, results) => {
+        if (error) throw error;
+        console.table(results);
+        mainMenu();
+    });
+};
+
+function addDepartment() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'department',
+                message: 'What is the name of this department?'
+            }
+        ])
+        .then((answer) => {
+
+            let sql = `INSERT INTO department (name)
+            VALUES (?)`;
+
+            let params = [answer.department]
+
+            db.query(sql, params, (error, results) => {
+                if (error) throw error;
+                mainMenu();
+            });
+        });
+};
+
+function addRole() {
+
+    let departmentQuery = `SELECT * FROM department`;
+    let departments
+
+    db.query(departmentQuery, (error, results) => {
+        if (error) throw error;
+        departments = results;
+
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'title',
+                    message: 'What is the name of this role?'
+                },
+                {
+                    type: 'input',
+                    name: 'salary',
+                    message: 'What is the salary for this role?',
+                    validate: (answer) => {
+                        if (isNaN(answer)) {
+                            return "Please enter a number";
+                        }
+                        return true;
+                    },
+                },
+                {
+                    type: 'list',
+                    name: 'department',
+                    message: 'Which department does this role belong to?',
+                    choices: function() {
+                        let choiceArray = departments.map(choice => choice.name);
+                    return choiceArray}
+                }
+            ])
+            .then((answers) => {
+                db.query(
+                    `INSERT INTO roles(title, salary, department_id)
+                VALUES ("${answers.title}", "${answers.salary}", 
+                (SELECT id FROM department WHERE name = "${answers.department}"))`
+                )
+                mainMenu();
+            })
+    });
+};
+
+function addEmployee() {
+    let sql;
 
     db.query(sql, (error, results) => {
         if (error) throw error;
